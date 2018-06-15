f59e4cc41895e066e65edb89e6387c761165a16c
Gideon Sanni <gideonyte@hotmail.com>
2018-06-15 11:49:45 +0100

    PieChart Algorithm works!

--- Panel/BusinessLogic/ChartsLogic/GeneratorChartLogic/AssembleChart.cs
+++ Panel/BusinessLogic/ChartsLogic/GeneratorChartLogic/AssembleChart.cs
@@ -1,4 +1,5 @@
-﻿using LiveCharts.Wpf;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
 using LiveCharts.Wpf.Charts.Base;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,7 @@ namespace Panel.BusinessLogic.ChartsLogic.GeneratorChartLogic
 {
     public static class AssembleChart
     {
+        public static Func<ChartPoint,string> PointLabel { get; set; }
         public static void ShowPlot(string SelectedChartType, List<List<double>> AllOrdinateSeriesInHours,
                                     Tuple<Axis, List<Series>> AxisSeriesTuple, Chart Chart)
         {
@@ -32,8 +34,18 @@ namespace Panel.BusinessLogic.ChartsLogic.GeneratorChartLogic
                     List<StackedColumnSeries> StackedColumnSeries = AxisSeriesTuple.Item2.Cast<StackedColumnSeries>().ToList();
                     for (int i = 0; i < AllOrdinateSeriesInHours.Count(); i++)
                     {
+                        StackedColumnSeries.Add
+                        (
+                            new StackedColumnSeries
+                            {
+                                Values = new ChartValues<double>(),
+                                StackMode = StackMode.Values,
+                                DataLabels = true
+                            }
+                        );
                         for (int j = 0; j < AllOrdinateSeriesInHours[i].Count; j++)
                         {
+                            
                             StackedColumnSeries[i].Values.Add(AllOrdinateSeriesInHours[i][j]);
                         }
                         Chart.Series.Add(StackedColumnSeries[i]);
@@ -54,31 +66,33 @@ namespace Panel.BusinessLogic.ChartsLogic.GeneratorChartLogic
                 //    Chart.AxisX.Add(AxisSeriesTuple.Item1);
                 //    break;
 
-                //case "Line":
-                //    LineSeries LineSeries = (LineSeries)AxisSeriesTuple.Item2;
-                //    for (int i = 0; i < AllOrdinateSeriesInHours.Count(); i++)
-                //    {
-                //        foreach (var seriesValue in AllOrdinateSeriesInHours[i])
-                //        {
-                //            LineSeries.Values.Add(seriesValue);
-                //        }
-                //        Chart.Series.Add(LineSeries);
-                //    }
-                //    Chart.AxisX.Add(AxisSeriesTuple.Item1);
-                //    break;
+                case "Line":
+                    List<LineSeries> LineSeries = AxisSeriesTuple.Item2.Cast<LineSeries>().ToList();
+                    for (int i = 0; i < AllOrdinateSeriesInHours[0].Count; i++)
+                    {
+                        LineSeries[0].Values.Add(AllOrdinateSeriesInHours[0][i]);
+                    }
+                    Chart.Series.Add(LineSeries[0]);
+                    Chart.AxisX.Add(AxisSeriesTuple.Item1);
+                    break;
 
-                //case "Pie":
-                //    PieSeries PieSeries = (PieSeries)AxisSeriesTuple.Item2;
-                //    for (int i = 0; i < AllOrdinateSeriesInHours.Count(); i++)
-                //    {
-                //        foreach (var seriesValue in AllOrdinateSeriesInHours[i])
-                //        {
-                //            PieSeries.Values.Add(seriesValue);
-                //        }
-                //        Chart.Series.Add(PieSeries);
-                //    }
-                //    Chart.AxisX.Add(AxisSeriesTuple.Item1);
-                //    break;
+                case "Pie":
+                    PointLabel = chtPt => string.Format($"{chtPt.Y} ({chtPt.Participation:P})");
+                    List<Series> SeriesCollection = AxisSeriesTuple.Item2;
+                    for (int i = 0; i < AllOrdinateSeriesInHours[0].Count; i++)
+                    {
+                        Chart.Series.Add
+                        (
+                            new PieSeries
+                            {
+                                DataLabels = true,
+                                LabelPoint = PointLabel,
+                                Values = new ChartValues<double>() { AllOrdinateSeriesInHours[0][i] }
+                            }
+                        );
+                    }
+                    Chart.AxisX.Add(AxisSeriesTuple.Item1);
+                    break;
 
                 default:
                     break;
